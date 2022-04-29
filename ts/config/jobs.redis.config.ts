ef9e4ac2a73a3e798b96d6df5ed030b502aa5f0c
GuilhermeSantos001 <luizgp120@hotmail.com>
2022-04-29 08:49:23 -0300

    Atualizado as configurações do redis

--- ts/config/jobs.redis.config.ts
+++ ts/config/jobs.redis.config.ts
@@ -9,6 +9,7 @@ import { ConnectionOptions } from 'bullmq';
 const connectionOptions: ConnectionOptions = {
   host: String(process.env.REDIS_HOST),
   port: Number(process.env.REDIS_PORT),
+  password: String(process.env.REDIS_PASSWORD),
   db: 5
 }
 
