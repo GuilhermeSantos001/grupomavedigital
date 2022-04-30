eb21791b39e7123da876634d834dfcf6cc0fb640
GuilhermeSantos001 <luizgp120@hotmail.com>
2022-04-29 23:41:04 -0300

    Fixed Cookies

--- ts/graphql/index.ts
+++ ts/graphql/index.ts
@@ -11,12 +11,11 @@ import typeDefs from '@/graphql/typeDefs';
 import resolvers from '@/graphql/resolvers';
 
 export const cookieOptions: CookieOptions = {
-  domain: 'localhost',
+  domain: process.env.NODE_ENV === "development" ? 'localhost' : ".grupomavedigital.com.br",
   maxAge: 1000 * 60 * 15, // would expire after 15 minutes
   httpOnly: true, // The cookie only accessible by the web server
-  sameSite: true, // Enforces the "SameSite" cookie attribute for Session Cookies.
-  secure: false, // Indicates if the cookie should only be transmitted over a secure HTTPS connection from the client
-  signed: false, // Indicates if the cookie should be signed
+  sameSite: "strict", // Enforces the "SameSite" cookie attribute for Session Cookies.
+  secure: process.env.NODE_ENV === "development" ? false : true, // Indicates if the cookie should only be transmitted over a secure HTTPS connection from the client
 };
 
 startedServer({ typeDefs, resolvers, context: { cookieOptions } });
